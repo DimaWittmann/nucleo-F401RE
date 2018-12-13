bd09545263c437260d7f579047e038854d270c1a
Dmytrii Vitman <dimawittmann@gmail.com>
2018-12-13 16:24:47 +0200

    Init a project for NUCLEO-F401RE.

---
+++ inc/philosophers.h
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define philoPHILOSOPHERS_NUMBER (5)
+#define philoCHECK_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
+#define notifLEFT_CHOPSTICK (0x1UL)
+#define notifRIGHT_CHOPSTICK (0x2UL)
+
+
+struct Chopstick {
+	unsigned int id;
+	bool available;
+};
+
+enum State {
+	NOOP,
+	EATING = 1,
+	HUNGRY,
+	THINKING,
+	STATE_MAX,
+};
+
+struct Philosopher {
+	unsigned int id;
+	enum State state;
+	struct Chopstick *chopsticks;
+	bool hasLeftChopstick;
+	bool hasRightChopstick;
+	unsigned int ate;
+	unsigned int cycles;
+	TaskHandle_t taskHandle;
+};
+
+
+struct FSMState {
+	enum State (*action)(struct Philosopher *p);
+};
+
+
+bool checkLeftChopstick(struct Philosopher *p);
+bool checkRightChopstick(struct Philosopher *p);
+
+void getLeftChopstick(struct Philosopher *p);
+void getRightChopstick(struct Philosopher *p);
+
+void putLeftChopstick(struct Philosopher *p);
+void putRightChopstick(struct Philosopher *p);
+
+const char *stateToChar(enum State);
+
+void logMessage(const char *fmt, ...);
+
+void initPhilosophers(void);
+void deinitPhilosophers(void);
+void FSMInit(struct FSMState states[STATE_MAX],
+	     struct Philosopher philosophers[philoPHILOSOPHERS_NUMBER]);
+void FSMNextStep(struct Philosopher *p, struct FSMState states[STATE_MAX]);
+
+enum State FSMNoOp(struct Philosopher *p);
+enum State FSMThinking(struct Philosopher *p);
+enum State FSMEating(struct Philosopher *p);
+enum State FSMHungry(struct Philosopher *p);
